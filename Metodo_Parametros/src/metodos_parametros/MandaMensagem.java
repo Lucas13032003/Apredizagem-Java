18940f36387d5979ad7e84f6d6b92a078e75ad12
Lucas13032003 <120221548+Lucas13032003@users.noreply.github.com>
2022-12-09 20:14:53 -0300

    nv commit

---
+++ Metodo_Parametros/src/metodos_parametros/MandaMensagem.java
@@ -0,0 +1,12 @@
+package metodos_parametros;
+
+public class MandaMensagem {
+	public void enviaMensagem() {
+		System.out.println("Bem-vindo ao Brasil");
+	}
+	public void mandaBoasVindas (String nome, int idade) {
+		System.out.printf("Ola, %s, voce"
+				+ " tem %d anos de idade", nome, idade);
+	}
+
+}
---
+++ Metodo_Parametros/src/metodos_parametros/Principal.java
@@ -0,0 +1,22 @@
+package metodos_parametros;
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		String nomeAluno;
+		int idadeAluno;
+		Scanner entrada = new Scanner (System.in);
+		MandaMensagem oi = new MandaMensagem();
+		
+		System.out.println("Entre como seu nome: ");
+		nomeAluno = entrada.nextLine();
+		
+		System.out.println("Entre como sua idade: ");
+		idadeAluno = entrada.nextInt();
+		oi.mandaBoasVindas(nomeAluno, idadeAluno);
+		
+		entrada.close();
+
+	}
+
+}
